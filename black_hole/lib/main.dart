252c218e982f42f18550f9fffc13548301f43c3e
Krishan <xp3cto95@gmail.com>
2023-11-18 01:47:37 +0530

    update screen 1,2,3

--- black_hole/lib/main.dart
+++ black_hole/lib/main.dart
@@ -11,8 +11,229 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "space app",
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          shadowColor: Colors.orangeAccent,
+          elevation: 4,
+          title: const Text("BLACK HOLE", 
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+            color: Colors.white,
+          ),
+          ),
+          actions: [
+            IconButton(onPressed: (){}, 
+            icon: const Icon(Icons.menu),
+            color: Colors.white,
+          ),
+          ],
+        ),
+
+        body:  SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text("SPACE DETAILS", style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                ),
+          
+                const SizedBox(
+                  height: 50,
+                ),
+          
+                Center(
+                  child: Image.asset("assets/space1.png",
+                    height: 300,
+                    scale: 2,
+                  ),
+                ),
+          
+                const SizedBox(
+                  height: 50,
+                ),
+          
+                const Text("Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+                ),
+
+
+                const SizedBox(
+                  height: 30,
+                ),
+
+                GestureDetector(
+                    onTap: (){},
+                   child: Center(
+                     child: Container(
+                        padding: const EdgeInsets.all(15),
+                          width: 230,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Colors.redAccent,
+                          ),
+                        child: const Center(
+                          child: Text("SPACE DETAILS",
+                          style: TextStyle(color: Colors.white),
+                          ),
+                        ), 
+                      ),
+                    ),
+                 ),
+
+              // -------seconde screen-------
+              
+              Center(
+                child: Image.asset("assets/space2.png",
+                height: 300,
+                scale: 2,
+                ),
+              ),
+
+              const Text("Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.orange,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.pink,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.purple,
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.blue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // --------third screen-------
+
+              Center(
+                child: Image.asset("assets/space3.png",
+                height: 300,
+                scale: 2,
+                ),
+              ),
+
+              const Text("Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+
+              const SizedBox(
+                height: 30,
+              ),
+
+              GestureDetector(
+                    onTap: (){},
+                   child: Center(
+                     child: Container(
+                        padding: const EdgeInsets.all(15),
+                          width: 230,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Colors.pink,
+                          ),
+                        child: const Center(
+                          child: Text("SPACE DETAILS",
+                          style: TextStyle(color: Colors.white),
+                          ),
+                        ), 
+                      ),
+                    ),
+                 ),
+
+
+              const SizedBox(
+                height: 30,
+              ), 
+
+              Container(
+                height: 2,
+                width: 500,
+                decoration: const BoxDecoration(
+                  color: Colors.white30,
+                ),
+              ),
+              
+              
+              const SizedBox(
+                height: 10,
+              ),
+
+              const Text("BLACK HOLE",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              const Text("Forem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Praesent auctor purus luctus enim egestas, ac scelerisque ante pulvinar. Donec ut rhoncus ex. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum.",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 10,
+                fontWeight: FontWeight.w300,
+              ),
+              ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
